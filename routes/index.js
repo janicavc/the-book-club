2d7b711280213fbe1b7e23823741eb945d37d89c
janicavc <crisostomo_janica@yahoo.com>
2023-07-11 14:12:26 -0700

    add authentication

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // force pick acct everytime
+    prompt: "select_account"
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/books',
+    failureRedirect: '/books'
+  }
+));
 
 module.exports = router;
