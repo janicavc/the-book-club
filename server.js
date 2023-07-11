2e83b739af7370430311402ddb9fe2a14f435162
janicavc <crisostomo_janica@yahoo.com>
2023-07-11 13:24:00 -0700

    add user schema

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// require dotenv
+require('dotenv').config();
+// connect to the database
+require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
