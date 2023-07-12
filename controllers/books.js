be2de12b9451264060ae6555a7bf20e7e49e179b
janicavc <crisostomo_janica@yahoo.com>
2023-07-12 11:39:46 -0700

    fixed the index function

--- controllers/books.js
+++ controllers/books.js
@@ -5,8 +5,8 @@ module.exports = {
 };
 
 async function index(req, res) {
-    const books = await Book.find({});
-    res.render('books/index', { title: 'All Books', books});
+    const book = await Book.find({});
+    res.render('books/index', { title: 'All Books', book});
 }
 
 function newBook(req, res) {
