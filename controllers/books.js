d177173a715724c5f3b2843b37fe19adc570ef27
janicavc <crisostomo_janica@yahoo.com>
2023-07-11 17:06:02 -0700

    add index to view

--- controllers/books.js
+++ controllers/books.js
@@ -1,7 +1,26 @@
 module.exports = {
-    new: newBook
+    new: newBook,
+    create,
+    index
 };
+
+async function index(req, res) {
+    const books = await Book.find({});
+    res.render('books/index', { title: 'All Books', books});
+}
 
 function newBook(req, res) {
     res.render('books/new', { errorMsg: '' });
+}
+
+async function create(req, res) {
+    const Book = require('../models/book');
+
+    try {
+        await Book.create(req.body);
+        res.redirect('/books/new');
+    } catch (err) {
+        console.log(err);
+        res.render('books/new', { errorMsg: err.message });
+    }
 }
--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const booksCtrl = require('../controllers/books');
-
+// all start with /books
+// GET /books/new
 router.get('/new', booksCtrl.new);
+// POST /books
+router.post('/', booksCtrl.create);
+// GET /books
+router.get('/', booksCtrl.index);
 
 module.exports = router;
